aeda89b2e0f833d7d53ec9fb57f421f622af65a0
Lukasz Pawlik <l.pawlik@selleo.com>
2021-05-23 17:56:44 +0200

    get services from cluster

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module lukep91/ecs-rebalancer
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.24.0
+require (
+	github.com/aws/aws-lambda-go v1.24.0
+	github.com/aws/aws-sdk-go-v2 v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ecs v1.4.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
+// func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (error) {
+// 	log.Printf("Cloudwatch event: %v", event.Detail)
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
-}
+// }
 
 func main() {
-	lambda.Start(HandleRequest)
+	// lambda.Start(HandleRequest)
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile("documobi"), config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ecs_client := ecs.NewFromConfig(cfg)
+	services, err := get_services(ecs_client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Service arns: ", services)
+}
+
+func get_services(ecs_client *ecs.Client) ([]string, error) {
+	services := make([]string, 1)
+	for {
+		response, err := ecs_client.ListServices(context.TODO(), &ecs.ListServicesInput{Cluster: aws.String("staging-portal-cluster")})
+		if err != nil {
+			log.Fatal(err)
+			return make([]string, 0), err
+		}
+		services = append(services, response.ServiceArns...)
+		if response.NextToken == nil {
+			break
+		}
+	}
+
+	return services, nil
 }
