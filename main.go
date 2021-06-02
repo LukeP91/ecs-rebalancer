b9a4a9fa8550d68ef5cb296b1c3f2deb34b861b3
Lukasz Pawlik <l.pawlik@selleo.com>
2021-06-02 22:16:24 +0200

    Add lambda handler

--- main.go
+++ main.go
@@ -2,37 +2,80 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 )
 
-// func HandleRequest(ctx context.Context, event events.CloudWatchEvent) (error) {
-// 	log.Printf("Cloudwatch event: %v", event.Detail)
+type ecsCloudWatchEvent struct {
+	ContainerInstanceArn string `json:"containerInstanceArn"`
+	AgentConnected       bool   `json:"agentConnected"`
+}
 
-// }
-var clusterName = os.Getenv("ECS_CLUSTER_NAME")
+func parseEcsCloudWatchEvent(event events.CloudWatchEvent) (ecsCloudWatchEvent, error) {
+	var ecsEvent ecsCloudWatchEvent
+	err := json.Unmarshal([]byte(event.Detail), &ecsEvent)
+	if err != nil {
+		return ecsCloudWatchEvent{}, err
+	}
 
-func main() {
-	// lambda.Start(HandleRequest)
-	cfg, err := config.LoadDefaultConfig(
-		context.TODO(),
-	)
+	return ecsEvent, nil
+}
+
+func HandleRequest(ctx context.Context, event events.CloudWatchEvent) error {
+	log.Printf("Cloudwatch event: %v", event.Detail)
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ecsClient := ecs.NewFromConfig(cfg)
-	services, err := getServices(ecsClient)
+	ecsEvent, err := parseEcsCloudWatchEvent(event)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = updateServices(ecsClient, services)
-	if err != nil {
-		log.Fatal(err)
+
+	response, err := ecsClient.DescribeContainerInstances(
+		ctx,
+		&ecs.DescribeContainerInstancesInput{
+			ContainerInstances: []string{ecsEvent.ContainerInstanceArn},
+			Cluster:            aws.String(clusterName),
+		},
+	)
+	numberOfInstances := len(response.ContainerInstances)
+	log.Println("Number of instances:", numberOfInstances)
+	if numberOfInstances != 0 {
+		containerInstance := response.ContainerInstances[0]
+		numberOfRunningTasks := containerInstance.RunningTasksCount
+		numberOfPendingTasks := containerInstance.PendingTasksCount
+
+		if numberOfRunningTasks == int32(0) && numberOfPendingTasks == int32(0) && ecsEvent.AgentConnected {
+			services, err := getServices(ecsClient)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = updateServices(ecsClient, services)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return nil
+		} else {
+			log.Println("Cluster does not require rebalacing.")
+			return nil
+		}
 	}
+	return nil
+}
+
+var clusterName = os.Getenv("ECS_CLUSTER_NAME")
+
+func main() {
+	lambda.Start(HandleRequest)
 }
 
 func getServices(client *ecs.Client) ([]string, error) {
